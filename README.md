9a9f98da95c301a5302ea11c9aea6d83b1323e95
Rena Rallis <renarallis@msn.com>
2017-06-09 15:48:44 -0700

    continue to use console.log to fix issues as well as update formats

--- README.md
+++ README.md
@@ -33,27 +33,10 @@ It will return the user input number if the number is divisible by 3, 5, or 15.
  - Input: 15
  - Output: false
 
+ {Known Bugs: Console log indicates no syntax errors, however, user input will not display. Continue adding specs once first spec issue is resolved.}
 
-
-
-
-
-
-
-
-must reject numbers above 3999
-must reject negative numbers
-must not accept non-numerical symbols
-must return clear error message for any failed input conditions
-must return appropriate roman numerals for thousand's place
-must return appropriate roman numerals for hundred's place
-must return appropriate roman numerals for ten's place
-must return appropriate roman numerals for one's place
-Support and contact details
 
 _For further information or error reporting send messages to mailto:renarallis@msn.com _
-
-Technologies Used
 
 {Technologies used: GitHub, HTML, Bootstrap, CSS, JavaScript, jQuery.}
 
--- js/scripts.js
+++ js/scripts.js
@@ -7,22 +7,31 @@ $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
     var userinput = parseInt($("input#userinput").val());
-    $("#result").show();
-    console.log ("#result")
-    if (userinput / 3) {
-      $("ping").show();
-      // console.log (userinput)
-    } else if (userinput / 5) {
+    // var ping = (userinput / 3);
+    // var pong = userinput / 5;
+    // var pingpong = userinput / 15;
+    // console.log (pingpong)
+    // function range(start, end) {
+    //   return range.reverse(1, userinput);
+    // }
+    $("result").show();
+
+    if (userinput <= 0) {
+      $("#result").show();
+    } else if ("userinput" / 3) {
+      $("#ping").show();
+    //   // console.log (userinput)
+    } else if ("userinput" / 5) {
       $("pong").show();
-    } else if (userinput / 15) {
+    } else if ("userinput" / 3 * 5) {
       $("ping-pong").show();
     }
     // else if (userinput >= 1) {
     //   $("result").show()
-      // console.log(userinput)
+    //   // console.log(userinput)
+
   });
 });
-
 
 
     // var userinput = userinput.toString();
