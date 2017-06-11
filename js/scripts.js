26b3fb21d91eea8c4c06cd2c5e7789268488ddb4
Rena Rallis <renarallis@msn.com>
2017-06-10 18:28:46 -0700

    create basic functionality. To be done - clean up comments, check indentation

--- js/scripts.js
+++ js/scripts.js
@@ -14,19 +14,36 @@ $(document).ready(function() {
     // function range(start, end) {
     //   return range.reverse(1, userinput);
     // }
-    $("result").show();
-
-    if (userinput <= 0) {
-      $("#result").show();
-      else if (operator === "divide")
-    } else if ("userinput" / 3) {
-      $("#ping").show();
-    //   // console.log (userinput)
-    } else if ("userinput" / 5) {
-      $("pong").show();
-    } else if ("userinput" / 3 * 5) {
-      $("ping-pong").show();
+    for (i = 1; i <= userinput; i += 1) {
+      var output = i;
+      if (i % 3 === 0) {
+        // if divisible by 3 output ping
+        output = "ping";
+      }
+      if (i % 5 === 0) {
+        // if divisible by 5 output pong
+        output = "pong";
+      }
+      if (i % 15 === 0) {
+        // if divisible by 15 output pong
+        output = "ping-pong";
+      }
+      $("ul#result").append("<li>" + output + "</li>");
     }
+
+    // $("#result").text(userinput);
+
+    // if (userinput <= 0) {
+    //   $("#result").show();
+    //   else if (operator === "divide")
+    // } else if ("userinput" / 3) {
+    //   $("#ping").show();
+    // //   // console.log (userinput)
+    // } else if ("userinput" / 5) {
+    //   $("pong").show();
+    // } else if ("userinput" / 3 * 5) {
+    //   $("ping-pong").show();
+    // }
     // else if (userinput >= 1) {
     //   $("result").show()
     //   // console.log(userinput)
