10afc0568f84d2b1c5a0ff5d86470dc0d8909520
Rena Rallis <renarallis@msn.com>
2017-06-15 00:13:54 -0700

    check indentation of else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,10 @@ function processPingPong(theNumber) {
   if (theNumber % 15 === 0) {
     // if divisible by 15 output pong
     output = "ping-pong";
-  }
-  else if (theNumber % 3 === 0) {
+  }   else if (theNumber % 3 === 0) {
     // if divisible by 3 output ping
     output = "ping";
-  }
-  else if (theNumber % 5 === 0) {
+  }  else if (theNumber % 5 === 0) {
     // if divisible by 5 output pong
     output = "pong";
   }
